9edcedbe95fb1ac867de05ab59f449fdc3f947dc
musicmtr <music7mtr@mail.ru>
2021-02-01 16:41:06 +0300

    фикстура

---
+++ fixture/application.py
@@ -0,0 +1,27 @@
+from selenium.webdriver.firefox.webdriver import WebDriver
+from fixture.group import GroupHelper
+from fixture.session import SessionHelper
+from fixture.contact import ContactHelper
+from fixture.navigation import Navigation_Helper
+
+
+class Application:
+
+    def __init__(self):
+        self.wd = WebDriver()
+        self.wd.implicitly_wait(5)
+        self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
+        self.contact = ContactHelper(self)
+        self.navigation = Navigation_Helper(self)
+        self.wd.maximize_window()
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+    def destroy(self):
+        self.wd.quit()
