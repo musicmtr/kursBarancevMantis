8046cf16e66f7753e3215e79bb5e9a00beac3b94
musicmtr <music7mtr@mail.ru>
2021-02-03 19:27:33 +0300

    создание новых проектов рандомно

--- test/test_add_project.py
+++ test/test_add_project.py
@@ -6,6 +6,6 @@ def test_add_project(app):
     app.session.login("administrator", "root")
     assert app.session.is_logged_in_as("administrator")
     app.project.open_create_new_project()
-    app.project.fill_form_project(Project(name_project="TESTname", status="stable",
+    app.project.fill_form_project(Project(name_project=str(uuid.uuid4().hex), status="stable",
                                           description="TESTdescription", view_status="private"))
     app.project.confirm_add_project()
