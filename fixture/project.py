35cedf9af80ec23035d31e61d1a4da94c0e51c53
musicmtr <music7mtr@mail.ru>
2021-02-03 19:21:38 +0300

    нужен рандом для названия проджекта

--- fixture/project.py
+++ fixture/project.py
@@ -1,4 +1,5 @@
 import time
+from selenium.webdriver.support.select import Select
 from model.project import Project
 
 class ProjectHelper:
@@ -15,9 +16,14 @@ class ProjectHelper:
     def fill_form_project(self, project):
         wd = self.app.wd
         self.change_field_value(field_name='name', text=project.name_project)
+        wd.find_element_by_name("status").click()
+        Select(wd.find_element_by_name("status")).select_by_visible_text(project.status)
+        time.sleep(5)
+        wd.find_element_by_css_selector("input[name='inherit_global']").click()
+        wd.find_element_by_name("view_state").click()
+        Select(wd.find_element_by_name("view_state")).select_by_visible_text(project.view_status)
         self.change_field_value(field_name='description', text=project.description)
         time.sleep(5)
-
 
     def change_field_value(self, text, field_name):
         wd = self.app.wd
@@ -26,5 +32,12 @@ class ProjectHelper:
             wd.find_element_by_name(field_name).clear()
             wd.find_element_by_name(field_name).send_keys(text)
             # wd.find_element_by_css_selector(field_name).click()
+
+    def confirm_add_project(self):
+        wd = self.app.wd
+        wd.find_element_by_css_selector("input[value='Add Project']").click()
+
+
+
 
 
--- test/test_add_project.py
+++ test/test_add_project.py
@@ -1,8 +1,11 @@
 from model.project import Project
+import uuid
+
 
 def test_add_project(app):
     app.session.login("administrator", "root")
     assert app.session.is_logged_in_as("administrator")
     app.project.open_create_new_project()
-    app.project.fill_form_project(Project(name_project="TESTname"))
-    app.project.fill_form_project(Project(description="TESTdescription"))
+    app.project.fill_form_project(Project(name_project="TESTname", status="stable",
+                                          description="TESTdescription", view_status="private"))
+    app.project.confirm_add_project()
